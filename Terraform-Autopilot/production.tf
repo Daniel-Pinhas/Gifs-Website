495d2ecd25bc0282026da16cb2b82b9bd53c7dbb
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-07 10:07:27 +0300

    Modified Prod TF file

--- Terraform-Autopilot/production.tf
+++ Terraform-Autopilot/production.tf
@@ -1,24 +1,27 @@
-provider "google" {
-  credentials = ""
-  project = "lofty-dynamics-393510"
-  region  = "us-central1"
-}
+#provider "google" {
+ # credentials = ""
+  #project = "lofty-dynamics-393510"
+  #region  = "us-central1"
+#}
 
-terraform {
-    backend "gcs" {
-        bucket = "gifs-website-bucket"
-        prefix = "terraform/state"
-  }
-}
+#terraform {
+ #   backend "gcs" {
+  #      bucket = "gifs-website-bucket"
+   #     prefix = "terraform/state"
+  #}
+#}
 
-resource "google_compute_network" "vpc" {
-  name                    = local.network_name
-  auto_create_subnetworks = false
-}
+#resource "google_compute_network" "vpc" {
+ # name                    = 
+#local.network_name
+ # auto_create_subnetworks = false
+#}
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = local.subnet_name
-  region        = "us-central1"
-  network       = google_compute_network.vpc.self_link
-  ip_cidr_range = "10.10.0.0/24"
-}
+#resource "google_compute_subnetwork" 
+#"subnet" {
+ # name          = local.subnet_name
+ # region        = "us-central1"
+ # network       = 
+#google_compute_network.vpc.self_link
+ # ip_cidr_range = "10.10.0.0/24"
+#}
