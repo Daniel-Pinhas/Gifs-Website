f36fae1efb6551c3b20fee72bc2acf380cc10ce7
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-08 11:09:16 +0300

    Added import-test-cluster.tf

---
+++ Terraform-Autopilot/imports tfs/import-test-cluster.tf
@@ -0,0 +1,46 @@
+import {
+  to = google_container_cluster.primary
+  id = "projects/lofty-dynamics-393510/locations/us-central1/clusters/gifs-website-test"
+}
+
+resource "google_container_cluster" "primary" {
+  name             = "gifs-website-test"
+  location         = "us-central1"
+  network          = google_compute_network.vpc.name
+  subnetwork       = google_compute_subnetwork.subnet.name
+  enable_autopilot = true
+}
+
+import {
+  to = google_compute_network.vpc
+  id = "projects/lofty-dynamics-393510/global/networks/lofty-dynamics-393510-vpc"
+}
+
+import {
+  to = google_compute_subnetwork.subnet
+  id = "projects/lofty-dynamics-393510/regions/us-central1/subnetworks/lofty-dynamics-393510-subnet"
+}
+
+provider "google" {
+  credentials = "/Users/daniel/Documents/DevOps/tkn/lofty-dynamics-393510-2b63cc077c5f.json"
+  project     = "lofty-dynamics-393510"
+  region      = "us-central1"
+}
+
+
+locals {
+  network_name = "lofty-dynamics-393510-vpc"
+  subnet_name  = "lofty-dynamics-393510-subnet"
+}
+
+resource "google_compute_network" "vpc" {
+  name                    = local.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = local.subnet_name
+  region        = "us-central1"
+  network       = google_compute_network.vpc.self_link
+  ip_cidr_range = "10.10.0.0/24"
+}
