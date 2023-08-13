e6eba483cddd768a4f2c4829a66ba4659ad78f2b
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-13 14:21:43 +0300

    M

--- charts-prod/changeTagflask.sh
+++ charts-prod/changeTagflask.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Function to get the latest tag number from the Docker images
-get_latest_tag_number() {
-    repository=$1
-    latest_tag=$(docker images --format "{{.Repository}} {{.Tag}}" | grep "$repository" | grep -v "latest" | awk '{print $2}' | sort -V | tail -1)
-    echo "$latest_tag"
-}
-
-# Update the YAML file with the latest tag number
+# Function to update the YAML file with the latest tag
 update_yaml_file() {
     yaml_file=$1
     service=$2
@@ -15,17 +8,24 @@ update_yaml_file() {
     sed -i "s|image: $service:.*|image: $service:$tag_number|" "$yaml_file"
 }
 
-# Main script
-repositories=("danielpinhas/flask-k8s" "danielpinhas/flask2-k8s" "danielpinhas/flask3-k8s")
+# Check if repository argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <repository>"
+    exit 1
+fi
 
-for repo in "${repositories[@]}"; do
-    latest_tag=$(get_latest_tag_number "$repo")
-    if [ -n "$latest_tag" ]; then
-        echo "Latest tag for $repo: $latest_tag"
-        sed_tag="${latest_tag//./\\.}"  # Escape dots for use in sed regex
-        yaml_file="flask-prod.yml"
-        update_yaml_file "$yaml_file" "$repo" "$latest_tag"
-    else
-        echo "Failed to retrieve latest tag for $repo"
-    fi
-done
+# Repository from command-line argument
+repository="$1"
+yaml_file="flask-prod.yml"
+
+# Retrieve the latest tag for the specified repository
+response=$(curl -s "https://registry.hub.docker.com/v2/repositories/$repository/tags/")
+latest_tag=$(echo "$response" | jq -r '.results | max_by(.last_updated) | .name')
+
+if [ -n "$latest_tag" ]; then
+    echo "Latest tag for $repository: $latest_tag"
+    sed_tag="${latest_tag//./\\.}"  # Escape dots for use in sed regex
+    update_yaml_file "$yaml_file" "$repository" "$latest_tag"
+else
+    echo "Failed to retrieve latest tag for $repository"
+fi
