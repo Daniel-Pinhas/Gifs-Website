519ac31f2cd20a36a3200a25e86620466f4f054c
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-14 14:05:16 +0300

    M

--- charts-prod/updateHTML.sh
+++ charts-prod/updateHTML.sh
@@ -4,8 +4,8 @@
 ingress_ips=($(kubectl describe service | grep 'LoadBalancer Ingress:' | awk '{print $3}'))
 
 # Replace the placeholders with the Ingress IPs in the YAML file
-sed -i "s|__FLASK_IP_PLACEHOLDER_1__|http://${ingress_ips[0]}:5000|g" your-yaml-file.yaml
-sed -i "s|__FLASK_IP_PLACEHOLDER_2__|http://${ingress_ips[1]}:5000|g" your-yaml-file.yaml
-sed -i "s|__FLASK_IP_PLACEHOLDER_3__|http://${ingress_ips[2]}:5000|g" your-yaml-file.yaml
+sed -i "s|__FLASK_IP_PLACEHOLDER_1__|http://${ingress_ips[0]}:5000|g" 'your-yaml-file.yaml'
+sed -i "s|__FLASK_IP_PLACEHOLDER_2__|http://${ingress_ips[1]}:5000|g" 'your-yaml-file.yaml'
+sed -i "s|__FLASK_IP_PLACEHOLDER_3__|http://${ingress_ips[2]}:5000|g" 'your-yaml-file.yaml'
 
 echo "Ingress IPs replaced in the YAML file successfully."
