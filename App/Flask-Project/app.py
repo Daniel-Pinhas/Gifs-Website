fa1e48209040520986ac602e96c25ee562139448
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-24 14:51:33 +0300

    M

--- App/Flask-Project/app.py
+++ App/Flask-Project/app.py
@@ -32,4 +32,4 @@ def index():
     return render_template("index.html", url=url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
