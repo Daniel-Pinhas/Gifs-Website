1e962ae570df698da5ca8bff8bfa4a62e54c66e1
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-28 14:49:12 +0300

    M

--- k8s-prod/updateConfig.py
+++ k8s-prod/updateConfig.py
@@ -13,9 +13,9 @@ config_path = "config.yml"
 with open(config_path, "r") as f:
     content = f.read()
 
-content = content.replace(f"http://{placeholder_1}:5001", f"http://{ingress_ips[0]}:5001")
-content = content.replace(f"http://{placeholder_2}:5002", f"http://{ingress_ips[1]}:5002")
-content = content.replace(f"http://{placeholder_3}:5003", f"http://{ingress_ips[2]}:5003")
+content = content.replace(f"http://{placeholder_1}:80", f"http://{ingress_ips[0]}:80")
+content = content.replace(f"http://{placeholder_2}:80", f"http://{ingress_ips[1]}:80")
+content = content.replace(f"http://{placeholder_3}:80", f"http://{ingress_ips[2]}:80")
 
 with open(config_path, "w") as f:
     f.write(content)
