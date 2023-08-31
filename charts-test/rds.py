2077de85cbeae71396cb55238c0c8729f79e7140
Daniel-Pinhas <danielp1239@gmail.com>
2023-08-31 09:19:22 +0300

    M

---
+++ charts-test/rds.py
@@ -0,0 +1,15 @@
+import os
+
+rds_creds = os.environ.get("RDS_CREDS")
+
+# Read the file content
+with open("values.yml", "r") as f:
+    file_content = f.read()
+
+# Replace placeholders with the secret value
+file_content = file_content.replace("$DEV_RDS_CREDS_U", rds_creds)
+file_content = file_content.replace("$DEV_RDS_CREDS_P", rds_creds)
+
+# Write the modified content back to the file
+with open("values.yml", "w") as f:
+    f.write(file_content)
