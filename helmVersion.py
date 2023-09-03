4ff684c3af02485ae9c8aea9644f50ab4005090c
Daniel-Pinhas <danielp1239@gmail.com>
2023-09-03 16:32:05 +0300

    M

---
+++ helmVersion.py
@@ -0,0 +1,47 @@
+import os
+import yaml
+
+# Define the path to your Chart.yaml file
+chart_yaml_path = "Chart.yaml"
+
+# Read the YAML file
+with open(chart_yaml_path, "r") as f:
+    chart_yaml = yaml.safe_load(f)
+
+# Extract the "version" field
+if "version" in chart_yaml:
+    version = chart_yaml["version"]
+    # Split the version string into its components (major, minor, and patch)
+    major, minor, patch = map(int, version.split('.'))
+    
+    # Check if the minor is 9 and patch is 9
+    if minor == 9 and patch == 9:
+        # If both are 9, increment the major, reset minor and patch to 0
+        major += 1
+        minor = 0
+        patch = 0
+    # Check if the minor is 9 but patch is not 9
+    elif minor == 9:
+        # If minor is 9 but patch is not, increment the minor and reset patch to 0
+        minor += 1
+        patch = 0
+    else:
+        # Otherwise, increment the patch
+        patch += 1
+
+    # Update the version in the dictionary
+    chart_yaml["version"] = f"{major}.{minor}.{patch}"
+    
+    # Print the updated version
+    updated_version = chart_yaml["version"]
+    print(f"Updated Version: {updated_version}")
+
+    # Export the updated version as an environment variable
+    os.environ["Updated-Version"] = f"Updated Version: {updated_version}"
+    
+    # Write the updated YAML back to the file
+    with open(chart_yaml_path, "w") as f:
+        yaml.dump(chart_yaml, f, default_flow_style=False)
+
+else:
+    print("Version field not found in Chart.yaml")
