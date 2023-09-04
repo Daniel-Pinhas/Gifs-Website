fd7339d04545befd250dff4cd68058db49263966
Daniel-Pinhas <danielp1239@gmail.com>
2023-09-04 17:50:32 +0300

    M

--- valuesDel.py
+++ valuesDel.py
@@ -1,13 +1,20 @@
+import os
+
 file_path = "helm-chart/values.yml"
-lines_to_delete = ["  user_name:", "  user_pwd:"]
+lines_to_delete = ["user_name:", "user_pwd:"]
 
-# Read the file
-with open(file_path, "r") as file:
-    lines = file.readlines()
+# Check if the file exists
+if os.path.isfile(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
 
-# Remove the specified lines
-new_lines = [line for line in lines if not any(line.strip().startswith(pattern) for pattern in lines_to_delete)]
+    # Remove the specified lines
+    new_lines = [line for line in lines if not any(line.strip().startswith(pattern) for pattern in lines_to_delete)]
 
-# Write the updated content back to the file
-with open(file_path, "w") as file:
-    file.writelines(new_lines)
+    # Write the updated content back to the file
+    with open(file_path, "w") as file:
+        file.writelines(new_lines)
+
+    print("Lines removed successfully.")
+else:
+    print(f"File not found: {file_path}")
